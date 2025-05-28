7364a7806b3f712d1a244fb8248546a34f19e1bc
ValenNz <nuevalenalswando@gmail.com>
2025-05-28 11:59:23 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,25 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Dashboard - LIBRAIN')</title>
+    <title>@yield('title')</title>
 
-    <script src="https://cdn.tailwindcss.com "></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons @1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     @stack('styles')
 </head>
 <body class="bg-gray-100 min-h-screen flex">
 
-    @unless(request()->is('login'))
-
+    @auth
         @include('components.sidebar')
+        <div class="ml-64 flex-1 p-8">
+            @yield('content')
+        </div>
+    @endauth
 
-    @endunless
-
-    <div class="ml-64 flex-1 p-8">
-        @yield('content')
-    </div>
+    @guest
+        <div class="w-full">
+            @yield('content')
+        </div>
+    @endguest
 
     @stack('scripts')
 </body>
